76a76def398ec9004648f298ec2e4a72120508a4
carraes <carraeshb@gmail.com>
2022-12-25 14:43:44 -0300

    feat: linked lists sort

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
 const numSort = new Sorter(numbersCollection)
@@ -11,3 +12,12 @@ const charCollection = new CharactersCollection('Xaayb')
 const charSort = new Sorter(charCollection)
 charSort.sort()
 console.log(charCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+const sortL = new Sorter(linkedList)
+sortL.sort()
+linkedList.print()
