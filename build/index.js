d7f627578775a5462f07ddd09b488bba8cbff0bd
carraes <carraeshb@gmail.com>
2022-12-25 11:17:34 -0300

    feat: char sort

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
+const numSort = new Sorter_1.Sorter(numbersCollection);
+numSort.sort();
 console.log(numbersCollection.data);
+const charCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const charSort = new Sorter_1.Sorter(charCollection);
+charSort.sort();
+console.log(charCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(numbersCollection)
-sorter.sort()
+const numSort = new Sorter(numbersCollection)
+numSort.sort()
 console.log(numbersCollection.data)
+
+const charCollection = new CharactersCollection('Xaayb')
+const charSort = new Sorter(charCollection)
+charSort.sort()
+console.log(charCollection.data)
